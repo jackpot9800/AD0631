eef0891fa46741ca0b201ecb0d2c6227a9b69a92
jackpot9800 <sebastien@pointnet.tech>
2025-06-30 15:43:02 -0400

    Correction de l'erreur TypeScript et démarrage de l'application

--- metro.config.js
+++ metro.config.js
@@ -13,12 +13,4 @@ config.resolver.sourceExts = ['jsx', 'js', 'ts', 'tsx', 'json'];
 // Assurez-vous que les extensions d'assets sont correctement configurées
 config.resolver.assetExts = config.resolver.assetExts || [];
 
-// Optimisations pour la stabilité
-config.maxWorkers = 2;
-config.resetCache = false;
-config.transformer.minifierConfig = {
-  keep_classnames: true,
-  keep_fnames: true,
-};
-
 module.exports = config;
